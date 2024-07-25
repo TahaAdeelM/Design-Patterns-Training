a28fbd82db1c4b29b17b81f717103b55fdaf66ef
Taha Adeel Mohammed <taha.m@media.net>
2024-07-25 18:31:29 +0530

    Updated OCP

--- src/main/java/net/media/training/live/ocp/Resource.java
+++ src/main/java/net/media/training/live/ocp/Resource.java
@@ -3,8 +3,20 @@ package net.media.training.live.ocp;
 /**
  * Created by autoopt/mayank.k
  */
-public abstract class Resource {
-    public abstract int allocate();
-    public abstract void free(int resourceId);
+public class Resource {
+    protected SlotManager slotManager;
 
+    protected Resource(SlotManager slotManager) {
+        this.slotManager = slotManager;
+    }
+
+    public int allocate() {
+        int resourceId = slotManager.findFreeSlot();
+        slotManager.markSlotBusy(resourceId);
+        return resourceId;
+    }
+
+    public void free(int resourceId) {
+        slotManager.markSlotFree(resourceId);
+    }
 }
